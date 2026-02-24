8da7f905e8f82d08679f28197676ad4fe6eeb632
Samer <samer.0280alhamwi@gmail.com>
2026-02-24 14:07:44 +0300

    .

--- create-db.sh
+++ create-db.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Check if a database name was provided
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
     echo "Usage: ./create-db.sh <DatabaseName>"
     exit 1
 fi
@@ -10,23 +8,25 @@ fi
 DB_NAME=$1
 SA_PASSWORD="YourSecurePassword123!"
 
-echo "Searching for the database container..."
-
-# Try to find the container ID using docker compose
+# Find container
 CONTAINER_ID=$(docker compose ps -q db)
-
-if [ -z "$CONTAINER_ID" ]
-then
-    echo "Error: The database container ('db' service) is not running."
-    echo "Please run 'docker compose up -d' first and wait a few seconds."
+if [ -z "$CONTAINER_ID" ]; then
+    echo "Error: Database container not found."
     exit 1
 fi
 
-echo "Creating database: $DB_NAME inside container $CONTAINER_ID..."
+# Try to find sqlcmd in common paths
+SQLCMD_PATH="/opt/mssql-tools18/bin/sqlcmd"
+if ! docker exec $CONTAINER_ID test -f $SQLCMD_PATH; then
+    SQLCMD_PATH="/opt/mssql-tools/bin/sqlcmd"
+fi
 
-# Execute SQL Command
-docker exec -i $CONTAINER_ID /opt/mssql-tools/bin/sqlcmd \
-   -S localhost -U sa -P $SA_PASSWORD \
+echo "Using sqlcmd at: $SQLCMD_PATH"
+echo "Creating database: $DB_NAME..."
+
+# Execute with -C (Trust Server Certificate) which is required for tools v18+
+docker exec -i $CONTAINER_ID $SQLCMD_PATH \
+   -S localhost -U sa -P $SA_PASSWORD -C \
    -Q "IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'$DB_NAME') CREATE DATABASE [$DB_NAME];"
 
 if [ $? -eq 0 ]; then
